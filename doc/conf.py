f1ffa217b2de0c41eddddabd8cfa2612e615eac7
Lutz Gross <6036995+LutzGross@users.noreply.github.com>
2025-10-31 18:16:53 +1000

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -7,9 +7,9 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Example'
-copyright = 'workshop participant'
-author = 'workshop participant'
-release = '0.1'
+copyright = 'Hitzacker'
+author = 'LG'
+release = '1.1'
 
 
 # -- General configuration ---------------------------------------------------
